bb4787b32026f4dbf01a21f083552bc85d07bf13
Sunder Kumar <sunder-kirei@outlook.com>
2025-02-15 17:06:52 +0530

    add code execution logic and dependencies

--- worker/src/app.ts
+++ worker/src/app.ts
@@ -1,41 +1,54 @@
-import {
-  kafkaConnect,
-  kafkaConsumer,
-  kafkaDisconnect,
-  kafkaProducer,
-} from "./lib/kafka";
+import { kafkaConnect, kafkaConsumer, kafkaDisconnect } from "./lib/kafka";
+import { runC } from "./lib/runC";
 import { runCpp } from "./lib/runCpp";
+import { runJs } from "./lib/runJs";
+import { runPy } from "./lib/runPy";
 import { Language } from "./types/Language";
-import { Status } from "./types/Status";
+import { config } from "dotenv";
+
+config();
 
 export async function startWorker() {
-  await kafkaConnect();
+  try {
+    await kafkaConnect();
 
-  await kafkaConsumer.run({
-    eachMessage: async ({ message }) => {
-      const { value } = message;
-      if (!value) return;
-      const {
-        id,
-        code,
-        language,
-      }: { id: string; code: string; language: Language } = JSON.parse(
-        value.toString()
-      );
+    await kafkaConsumer.run({
+      eachMessage: async ({ message }) => {
+        const { value } = message;
+        if (!value) return;
+        const {
+          id,
+          code,
+          language,
+        }: { id: string; code: string; language: Language } = JSON.parse(
+          value.toString()
+        );
 
-      await runCpp({ code, id, language });
-    },
-    autoCommitInterval: 1000,
-  });
+        switch (language) {
+          case Language.CPP:
+            await runCpp({ code, id, language });
+            break;
+          case Language.C:
+            await runC({ code, id, language });
+            break;
+          case Language.PYTHON:
+            await runPy({ code, id, language });
+            break;
+          case Language.JAVASCRIPT:
+            await runJs({ code, id, language });
+            break;
+        }
+      },
+      autoCommitInterval: 1000,
+    });
+  } catch (error) {
+    console.log(error);
+    await kafkaDisconnect();
+    process.exit(1);
+  }
 }
 
-try {
-  startWorker();
-} catch (error) {
-  process.exit(1);
-} finally {
-  kafkaDisconnect();
-}
+startWorker();
 
 process.on("SIGINT", async () => {
   await kafkaDisconnect();
