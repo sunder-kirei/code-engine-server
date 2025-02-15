d29a26e03f03278f3320b76da716559980e89d7b
Sunder Kumar <sunder-kirei@outlook.com>
2025-02-15 17:05:01 +0530

    env types update

--- worker/src/env.d.ts
+++ worker/src/env.d.ts
@@ -4,10 +4,6 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV: "development" | "production";
-      DATABASE_URL: string;
-      CRYPTO_ITERATIONS: string;
-      CRYPTO_KEYLEN: string;
-      JWT_SECRET: string;
       KAFKA_CLIENT_ID: string;
       KAFKA_BROKERS: string;
     }
