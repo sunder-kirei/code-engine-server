de46dc4f21702ca1a848ef71398ea7f59ec4ffc5
Sunder Kumar <sunder-kirei@outlook.com>
2025-02-21 23:45:43 +0530

    fix redis_url

--- worker/src/app.ts
+++ worker/src/app.ts
@@ -16,6 +16,7 @@ export async function startWorker() {
       eachMessage: async ({ message }) => {
         const { value } = message;
         if (!value) return;
+
         const {
           id,
           code,
