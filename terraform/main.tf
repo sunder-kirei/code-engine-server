c5027ba1233aef179056a8e22934ed363071c81b
Sunder Kumar <sunder-kirei@outlook.com>
2025-03-06 19:07:14 +0530

    change bucket name

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-state-storage-code-engine-server"
+    bucket       = "code-engine-server-terraform-state"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
